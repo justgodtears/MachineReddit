3b65ef6668d826a0080cf1f5f8376063bbd41724
oli-dev-ai <oopyrchal@pm.me>
2026-07-07 18:36:35 +0200

    reading dataset and randomizing data with numpy

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import numpy as np
 conn = sqlite3.connect("dataset/reddit-comments-may-2015/database.sqlite")
 cursor = conn.cursor()
 
-df = pandas.read_sql_query("SELECT * FROM May2015 LIMIT 20000", conn)
+random_array = np.random.choice(range(54504410), 20000, replace=False)
+
+array_to_list = ",".join(map(str, random_array))
+
+df = pandas.read_sql_query(f"""SELECT * 
+                              FROM May2015 
+                              WHERE rowid IN ({array_to_list});
+                           """, conn)
+
+
 print(df.head())
 
 cursor.close()
